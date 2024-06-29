2cd76697d5b745d3c6f5b6a8e8cfea918bfe5127
Vuyani Ncube <nvuyani307@gmail.com>
2024-06-29 23:40:37 +0200

    adding a mutual funds page

---
+++ client/front_exchange/src/app/pages/mutual-funds/mutual-funds.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mutual-funds',
+  standalone: true,
+  imports: [],
+  templateUrl: './mutual-funds.component.html',
+  styleUrl: './mutual-funds.component.css'
+})
+export class MutualFundsComponent {
+
+}
